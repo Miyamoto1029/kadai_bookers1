383591a7fde99488f37d3724076fa707b2817911
Miyamoto1029 <musashimiymoto@gmail.com>
2018-10-11 18:45:00 +0900

    1回目の提出

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,45 @@
+class PostsController < ApplicationController
+  def new
+  @post = Post.new
+  @posts = Post.all
+  end
+
+  def create
+  	post = Post.new(post_params)
+  	post.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to post_path(post.id)
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to posts_new_path
+  end
+
+  private
+
+  def post_params
+  	  params.require(:post).permit(:title, :body)
+	end
+
+end
